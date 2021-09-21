5ea68123e1e31c3a41db24fc17457bdd4371e53c
Sakibulislam <88928705+SakibulislamQ@users.noreply.github.com>
2021-09-21 17:44:11 +0600

    Update Miter.py

--- Miter.py
+++ Miter.py
@@ -4,31 +4,47 @@
     1 inchi = 1 inchi
 '''
 
-class oupt:
+class Oupt:
     def __init__(miter, num, froms ,tos):
         miter.num = num
         miter.froms = froms
         miter.tos = tos
 
-    def conv_cm(miter, num, froms, tos):
-        if froms == 'meter':
-            main_num = num * 39.37007874
-        if froms == 'foot':
-            main_num = num * 12
-        if froms == 'cm':
-            main_num = num * 0.03280839895
-        if froms == 'inchi':
-            main_num = num * 1
+    def conv_cm(miter):
+        if miter.froms == 'meter':
+            main_num = miter.num * 39.37007874
+            return main_num
+        if miter.froms == 'foot':
+            main_num = miter.num * 12
+            return main_num
+        if miter.froms == 'cm':
+            main_num = miter.num * 0.03280839895
+            return main_num
+        if miter.froms == 'inchi':
+            main_num = miter.num * 1
+            return main_num
 
-    def conv_tos(miter, num, froms, tos):
-        if tos == 'meter':
+    def conv_tos(miter, main_num):
+        if miter.tos == 'meter':
             oupts = main_num / 39.37007874
-        if tos == 'foot':
+            return oupts
+        if miter.tos == 'foot':
             oupts = main_num / 12
-        if tos == 'cm':
+            return oupts
+        if miter.tos == 'cm':
             oupts = main_num / 0.03280839895
-        if tos == 'inchi':
+            return oupts
+        if miter.tos == 'inchi':
             oupts = main_num / 1
+            return oupts
+    
+
+mit = Oupt(50,'foot', 'meter')
+def out_put():
+    asas = mit.conv_tos(mit.conv_cm())
+    print(asas)
+out_put()
+
 
 
 
