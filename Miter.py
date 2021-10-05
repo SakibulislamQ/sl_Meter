47bf2d7883f8b7a1435ccf38b57db75f12492d5a
Sakibulislam <88928705+SakibulislamQ@users.noreply.github.com>
2021-10-05 15:40:37 +0600

    Update Miter.py

--- Miter.py
+++ Miter.py
@@ -32,36 +32,40 @@ def conv(num, froms, tos): # conv 'fron' to 'to'
 
 # making main window >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-from tkinter import * 
-from tkinter import ttk
+import tkinter
+from tkinter import Label
+from tkinter import Entry
+from tkinter import Button
+from tkinter.ttk import Combobox
+from tkinter import StringVar
 
-win = Tk() # making window 
+win = tkinter.Tk() # making window 
 # designimg window >>>>>
 win.title('Meter')
 win.iconbitmap('ac.ico')
 win.geometry('700x300')
 win.attributes('-topmost',1)
-win.configure(bg='SkyBlue')
+win.configure(bg='SkyBlue1')
 win.resizable(0,0)
 
 # making From text
-from_label = Label(win, text='From', font=('Arial',20),bg='SkyBlue', fg='red')
+from_label = Label(win, text='From', font=('Arial',20),bg='SkyBlue1', fg='red')
 from_label.place(x=120,y=40)
 
 # making To text
-to_label = Label(win, text='To', font=('Arial',20),bg='SkyBlue', fg='red')# From text 
+to_label = Label(win, text='To', font=('Arial',20),bg='SkyBlue1', fg='red')# From text 
 to_label.place(x=400,y=40)
 
 # making from combobox >>>>
 from_string = StringVar()
-from_combo = ttk.Combobox(win,width=30, state='readonly', textvariable=from_string)
+from_combo = Combobox(win,width=30, state='readonly', textvariable=from_string)
 from_combo['values']=('Meter','Feet','Centimetre','Inch')
 from_combo.current(0)# set the selected items
 from_combo.place(x=120,y=80)
 
 # making to combobox >>>>
 to_string = StringVar()
-to_combo = ttk.Combobox(win, width=30, state='readonly', textvariable=to_string)
+to_combo = Combobox(win, width=30, state='readonly', textvariable=to_string)
 to_combo['values']=('Meter','Feet','Centimetre','Inch')
 to_combo.current(1)# set the selected items
 to_combo.place(x=400,y=80)
@@ -79,19 +83,12 @@ def btn_cl():
     result = round(result,4)
     outp.configure(text=result)
 # making oupt
-outp = Label(win, text='', font=('Calibri Light bold',20), bg='SkyBlue', fg='red3')
+outp = Label(win, text='', font=('Calibri Light bold',20), bg='SkyBlue1', fg='red3')
 outp.place(x=320,y=210)
 # Go button
-go_button = Button(win, text='Go', font=('Arial bold', 12),bg='red', bd=0, command=btn_cl)
+go_button = Button(win, text='Go', font=('Arial bold', 12),bg='green2', bd=0, command=btn_cl)
 go_button.place(x=350,y=150)
 
 win.mainloop() # window mainloop
 # making main window <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-
-
-
-
-
-
 
